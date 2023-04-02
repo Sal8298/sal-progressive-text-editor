01b6da39307072f3f61ff37328664aee4012cf59
Sal8298 <salahuddinasim95@gmail.com>
2023-04-01 22:01:32 -0500

    Added logic to get and push from database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const putJateNoteDb = await openDB('jate', 1);
+  const tx = putJateNoteDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GETfrom the database');
+  const getJateNoteDb = await openDB('jate', 1);
+  const tx = getJateNoteDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  if (result) {
+    console.log('data was retreived!')
+  } else {
+    console.log('Data was not found. Try again!')
+  }
+  return result.value
+};
 
 initdb();
